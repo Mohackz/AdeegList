a1d26b2116522e3073f689097d625b469c4cbb1e
Mohackz <mohackz@live.fr>
2021-04-17 00:35:13 +0300

    Bismillah al-Rahman al-Rahim

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import "./index.css";
+
+const App = () => {
+	const [todos, setTodo] = useState([]);
+
+	return <div>This is the main component</div>;
+};
+
+export default App;
---
+++ src/components/Topbar/Topbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./topbar.style.css";
+
+const Topbar = () => {
+	return (
+		<nav id="app-nav">
+			<div id="app-logo"></div>
+			<div id="app-action"></div>
+		</nav>
+	);
+};
